91c9d76f42b89a37073765c377d77d702270dbb6
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-07-08 00:12:21 +0300

    Update Loader.tsx

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -5,8 +5,8 @@ export const Loader = () => {
     <div className="flex items-center justify-center fixed bg-[#0b0b0b99] backdrop-blur-sm w-full h-full left-0 top-0 z-50">
       <Rings
         visible={true}
-        height="100"
-        width="100"
+        height="180"
+        width="180"
         color="#4fa94d"
         ariaLabel="rings-loading"
         wrapperStyle={{}}
