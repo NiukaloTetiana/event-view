cb317053cd64d5e27a0b469d42347d853c51bad0
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-07-10 01:19:29 +0300

    add img for Home page

--- src/components/ScrollUpButton/ScrollUpButton.tsx
+++ src/components/ScrollUpButton/ScrollUpButton.tsx
@@ -33,11 +33,11 @@ export const ScrollUpButton = () => {
     <button
       onClick={scrollUp}
       className={`
-        flex justify-center items-center fixed right-4 bottom-4 md:right-[30px] md:bottom-[30px] lg:right-[60px] lg:bottom-[40px] rounded-full w-12 h-12 shadow-md bg-yellow-300 hover:bg-amber-200 transition duration-300 ${
+        flex justify-center items-center fixed right-4 bottom-4 md:right-[30px] md:bottom-[30px] lg:right-[60px] lg:bottom-[40px] rounded-full w-12 h-12 shadow-md bg-accentColor hover:bg-hoverColor transition duration-300 ${
           !isVisible ? "scale-0" : "scale-1"
         }`}
     >
-      <Icon id="arrow" className="fill-white" size="20" />
+      <Icon id="arrow" className="fill-whiteColor" size="20" />
     </button>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,45 @@
 import { useNavigate } from "react-router-dom";
 
 import { Icon } from "../components";
+import welcome from "../assets/images/welcome.png";
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="container py-8 lg:text-left">
-      <h1 className="md:w-[400px] lg:w-[760px] font-semibold text-[32px] md:text-[48px] lg:text-[60px] leading-[1.02] tracking-[-0.02em] mb-[20px]">
-        Open the world of events with{" "}
-        <span className="text-accentColor italic">Event View</span> - all events
-        in one place!
-      </h1>
-      <p className="md:w-[400px] lg:w-[510px] font-medium text-[14px] md:text-[16px] lg:text-[18px] leading-[1.33] tracking-[-0.02em] mb-[30px] md:mb-10">
-        Discover and manage all your events in one place with us. Stay updated,
-        plan efficiently, and never miss an event again.
-      </p>
-      <button
-        type="button"
-        onClick={() => navigate("/board")}
-        className="flex gap-[18px] justify-center items-center border border-transparent bg-accentColor rounded-[30px] py-[18px] w-[230px] font-medium text-[20px] text-bgFirstLigtColor
+    <div className="container py-8 flex flex-col gap-5 md:flex-row md:items-center md:justify-between">
+      <div className="md:w-[400px] lg:w-[560px]">
+        <h1 className="font-semibold text-[32px] md:text-[48px] lg:text-[80px] leading-[1.02] tracking-[-0.02em] mb-[30px]">
+          Open the world of events with{" "}
+          <span className="text-accentColor italic">Event View</span> - all
+          events in one place!
+        </h1>
+        <p className="font-medium text-[14px] md:text-[16px] lg:text-[18px] leading-[1.33] tracking-[-0.02em] mb-[30px] md:mb-10">
+          Discover and manage all your events in one place with us. Stay
+          updated, plan efficiently, and never miss an event again.
+        </p>
+        <button
+          type="button"
+          onClick={() => navigate("/board")}
+          className="flex gap-[18px] justify-center items-center border border-transparent bg-accentColor rounded-[30px] py-[18px] w-[230px] font-medium text-[20px] text-bgFirstLigtColor
       leading-[1.2] tracking-[-0.01em] primary-btn-hover group"
-      >
-        Get started
-        <Icon
-          id="arrow-up"
-          className="fill-current group-hover:rotate-[53deg] transition duration-500"
-          size="20"
-        />
-      </button>
+        >
+          Get started
+          <Icon
+            id="arrow-up"
+            className="fill-current group-hover:rotate-[53deg] transition duration-500"
+            size="20"
+          />
+        </button>
+      </div>
+
+      <img
+        // srcSet={`${welcome1x} 1x, ${welcome2x} 2x`}
+        src={welcome}
+        alt="People are resting at event"
+        width={318}
+        className="sm-max:w-[280px] rounded-[10px]"
+      />
     </div>
   );
 };
