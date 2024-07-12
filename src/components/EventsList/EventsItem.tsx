1987f8af16fc01e9b101a3cefdea5af0008817e8
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-07-12 18:20:34 +0300

    change styles of EventsItem and description in events.json

--- src/components/EventsList/EventsItem.tsx
+++ src/components/EventsList/EventsItem.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { format } from "date-fns";
+
 import { IEvent } from "../../types";
 
 interface IEventItemProps {
@@ -12,29 +13,34 @@ export const EventsItem: React.FC<IEventItemProps> = ({
   index,
 }) => {
   return (
-    <li className="flex flex-col justify-center items-center lg:items-start lg:flex-row gap-[30px] lg:gap-[24px] w-[320px] lg:w-[550px] bg-bgFirstLigtColor rounded-[24px] p-[34px] height-[318px] shadow-md transition-transform transform hover:scale-105 focus:scale-105">
-      <div className="flex shrink-0 h-[96px] w-[129px] lg:h-[120px] lg:w-[120px] p-[12px] border-2 border-secondBorderColor rounded-[30px]">
-        <img
-          src={logo_url}
-          alt={organizer}
-          className="rounded-[15px] lg:w-[96px] lg:h-[96px]"
-          width="105"
-          height="72"
-        />
-      </div>
-      <div>
-        <h3 className="font-medium text-center md:text-start text-[18px] md:text-[20px] lg:text-[24px] leading-[1] text-darkColor mb-[24px]">
-          {title}
-        </h3>
+    <li className="w-[320px] lg:w-[574px] bg-bgFirstLigtColor rounded-[24px] p-[34px] height-[318px] shadow-md transition-transform transform hover:scale-105 focus:scale-105">
+      <div className="flex flex-col-reverse items-center lg:items-start lg:flex-row lg:justify-between gap-[30px] lg:gap-[24px]">
+        <div>
+          <h3 className="font-medium text-center lg:text-start text-[18px] md:text-[20px] lg:text-[24px] leading-[1] text-darkColor mb-[24px]">
+            {title}
+          </h3>
 
-        <div className="flex flex-col gap-[8px] mb-[24px]">
-          <p className="item">{format(event_date, "PP")}</p>
-          <p className="item">
-            <span className="text-secondTextColor">Organizer: </span>
-            {organizer}
-          </p>
+          <div className="flex flex-col gap-[8px] mb-[24px]">
+            <p className="item">{format(event_date, "PP")}</p>
+            <p className="item">
+              <span className="text-secondTextColor">Organizer: </span>
+              {organizer}
+            </p>
+          </div>
         </div>
-        <p className="font-normal text-[16px] leading-[1.25] text-secondTextColor mb-[14px]">
+        <div className="flex shrink-0 h-[136px] w-[169px] p-[12px] border-2 border-secondBorderColor rounded-[30px]">
+          <img
+            src={logo_url}
+            alt={organizer}
+            className="rounded-[15px]"
+            width="140"
+            height="112"
+          />
+        </div>
+      </div>
+
+      <div>
+        <p className="w-full font-normal text-[16px] leading-[1.25] text-secondTextColor mb-[14px]">
           {description}
         </p>
 
