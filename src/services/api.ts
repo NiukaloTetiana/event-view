ac00d28e4bbdfa0463945453a1fbb45b07a4a590
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-07-26 23:27:32 +0300

    add addEventUser, getEventsUser, getCurrentUser, refreshUser, loginUser, registerUser, logOutUser,

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,16 +6,31 @@ import {
   IParticipantNoId,
 } from "../types";
 
-const BASE_URL = "https://event-view-backend.onrender.com/api";
-
-const instance = axios.create({
-  baseURL: BASE_URL,
-});
-
 interface IGetEventsParams {
   page: number;
   limit: number;
 }
+
+interface IUserCredentials {
+  name?: string;
+  email: string;
+  password: string;
+}
+
+const BASE_URL = "https://event-view-backend.onrender.com/api";
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
+
+export const setToken = (token: string) => {
+  instance.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+export const clearToken = () => {
+  instance.defaults.headers.common.Authorization = "";
+};
 
 export const getEvents = async (
   params: IGetEventsParams,
@@ -56,4 +71,81 @@ export const getParticipantsByEventId = async (id: string) => {
   const { data } = await instance.get(`/participants?eventId=${id}`);
 
   return data;
+};
+
+export const registerUser = async (credentials: IUserCredentials) => {
+  try {
+    const { data } = await instance.post("/users/register", credentials);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const loginUser = async (credentials: IUserCredentials) => {
+  try {
+    const { data } = await instance.post("/users/login", credentials);
+    setToken(data.accessToken);
+    localStorage.setItem("refreshToken", JSON.stringify(data.refreshToken));
+    localStorage.setItem("user", JSON.stringify(data.user));
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const refreshUser = async (token: string) => {
+  try {
+    const { data } = await instance.post("/users/refresh", {
+      refreshToken: token,
+    });
+    setToken(data.accessToken);
+
+    const user = await getCurrentUser();
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("refreshToken", JSON.stringify(data.refreshToken));
+    console.log("error");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const { data } = await instance.get("/users/current");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getEventsUser = async () => {
+  try {
+    const { data } = await instance.get("/users/events");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const addEventUser = async (eventId: string) => {
+  try {
+    const { data } = await instance.post("/users/events", { eventId });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const logOutUser = async () => {
+  try {
+    const { data } = await instance.post("/users/logout");
+    clearToken();
+    localStorage.setItem("refreshToken", JSON.stringify(""));
+    localStorage.removeItem("user");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
